f324a40aa3dcdb0e6c5d313e93f26854e737beb0
Ihor Kalnytskyi <ihor@kalnytskyi.com>
2019-01-07 17:45:57 +0200

    Rethink 'metadata' processor
    Guess what? Yep, `when` is removed from 'metadata' processor. A
    corresponding processor-wrapper should be used instead.

--- tests/ext/processors/test_metadata.py
+++ tests/ext/processors/test_metadata.py
@@ -1,7 +1,5 @@
 """Metadata processor test suite."""
 
-import os
-
 import pytest
 
 from holocron import app
@@ -10,28 +8,13 @@ from holocron.ext.processors import metadata
 
 @pytest.fixture(scope='function')
 def testapp():
-    return app.Holocron({})
+    return app.Holocron()
 
 
-@pytest.fixture(scope='function')
-def run_processor():
-    streams = []
-
-    def run(*args, **kwargs):
-        streams.append(metadata.process(*args, **kwargs))
-        return streams[-1]
-
-    yield run
-
-    for stream in streams:
-        with pytest.raises(StopIteration):
-            next(stream)
-
-
-def test_document(testapp, run_processor):
+def test_item(testapp):
     """Metadata processor has to work!"""
 
-    stream = run_processor(
+    stream = metadata.process(
         testapp,
         [
             {
@@ -51,11 +34,14 @@ def test_document(testapp, run_processor):
             'type': 'memoire',
         }
 
+    with pytest.raises(StopIteration):
+        next(stream)
 
-def test_document_untouched(testapp, run_processor):
-    """Metadata processor has to ignore documents if metadata isn't passed."""
 
-    stream = run_processor(
+def test_item_untouched(testapp):
+    """Metadata processor has to ignore items if no metadata are passed."""
+
+    stream = metadata.process(
         testapp,
         [
             {
@@ -70,15 +56,48 @@ def test_document_untouched(testapp, run_processor):
             'author': 'skywalker',
         }
 
+    with pytest.raises(StopIteration):
+        next(stream)
+
+
+@pytest.mark.parametrize('amount', [0, 1, 2, 5, 10])
+def test_item_many(testapp, amount):
+    """Metadata processor has to work with stream."""
+
+    stream = metadata.process(
+        testapp,
+        [
+            {
+                'content': 'the key is #%d' % i,
+                'author': 'luke',
+            }
+            for i in range(amount)
+        ],
+        metadata={
+            'author': 'yoda',
+            'type': 'memoire',
+        })
+
+    for i in range(amount):
+        assert next(stream) == \
+            {
+                'content': 'the key is #%d' % i,
+                'author': 'yoda',
+                'type': 'memoire',
+            }
+
+    with pytest.raises(StopIteration):
+        next(stream)
+
 
 @pytest.mark.parametrize('overwrite, author', [
     (True, 'yoda'),
     (False, 'skywalker'),
 ])
-def test_param_overwrite(testapp, run_processor, overwrite, author):
+def test_param_overwrite(testapp, overwrite, author):
     """Metadata processor has to respect overwrite option."""
 
-    stream = run_processor(
+    stream = metadata.process(
         testapp,
         [
             {
@@ -99,70 +118,11 @@ def test_param_overwrite(testapp, run_processor, overwrite, author):
             'type': 'memoire',
         }
 
-
-def test_param_when(testapp, run_processor):
-    """Metadata processor has to ignore non-targeted documents."""
-
-    stream = run_processor(
-        testapp,
-        [
-            {
-                'content': 'the way of the Force #1',
-                'source': os.path.join('posts', '1.md'),
-            },
-            {
-                'content': 'the way of the Force #2',
-                'source': os.path.join('pages', '2.md'),
-            },
-            {
-                'content': 'the way of the Force #3',
-                'source': os.path.join('posts', '3.md'),
-            },
-            {
-                'content': 'the way of the Force #4',
-                'source': os.path.join('pages', '4.md'),
-            },
-        ],
-        metadata={
-            'author': 'kenobi',
-        },
-        when=[
-            {
-                'operator': 'match',
-                'attribute': 'source',
-                'pattern': r'^posts.*$',
-            },
-        ])
-
-    assert next(stream) == \
-        {
-            'content': 'the way of the Force #1',
-            'source': os.path.join('posts', '1.md'),
-            'author': 'kenobi',
-        }
-
-    assert next(stream) == \
-        {
-            'content': 'the way of the Force #2',
-            'source': os.path.join('pages', '2.md'),
-        }
-
-    assert next(stream) == \
-        {
-            'content': 'the way of the Force #3',
-            'source': os.path.join('posts', '3.md'),
-            'author': 'kenobi',
-        }
-
-    assert next(stream) == \
-        {
-            'content': 'the way of the Force #4',
-            'source': os.path.join('pages', '4.md'),
-        }
+    with pytest.raises(StopIteration):
+        next(stream)
 
 
 @pytest.mark.parametrize('params, error', [
-    ({'when': [42]}, 'when: unsupported value'),
     ({'metadata': 42}, "metadata: 42 should be instance of 'dict'"),
     ({'overwrite': 'true'}, "overwrite: 'true' should be instance of 'bool'"),
 ])
