12a61ef6de8956f16c00e85377b41be408feb823
Ihor Kalnytskyi <ihor@kalnytskyi.com>
2019-01-07 19:29:31 +0200

    Rethink 'prettyuri' processor
    When? That's right! `when` parameter has been removed!

--- holocron/ext/processors/prettyuri.py
+++ holocron/ext/processors/prettyuri.py
@@ -1,27 +1,18 @@
 """Strip .HTML extension from URIs."""
 
 import os
-import schema
-
-from ._misc import iterdocuments_ex, parameters
 
 
-@parameters(
-    schema={
-        'when': schema.Or([{str: object}], None, error='unsupported value'),
-    }
-)
-def process(app, documents, *, when=None):
-    for document, is_matched in iterdocuments_ex(documents, when):
-        if is_matched:
-            destination = os.path.basename(document['destination'])
+def process(app, stream):
+    for item in stream:
+        destination = os.path.basename(item['destination'])
 
-            # Most modern HTTP servers implicitly serve one of these files when
-            # requested URL is pointing to a directory on filesystem. Hence in
-            # order to provide "pretty" URLs we need to transform destination
-            # address accordingly.
-            if destination not in ('index.html', 'index.htm'):
-                document['destination'] = os.path.join(
-                    os.path.splitext(document['destination'])[0], 'index.html')
+        # Most modern HTTP servers implicitly serve one of these files when
+        # requested URL is pointing to a directory on filesystem. Hence in
+        # order to provide "pretty" URLs we need to transform destination
+        # address accordingly.
+        if destination not in ('index.html', 'index.htm'):
+            item['destination'] = os.path.join(
+                os.path.splitext(item['destination'])[0], 'index.html')
 
-        yield document
+        yield item
--- tests/ext/processors/test_prettyuri.py
+++ tests/ext/processors/test_prettyuri.py
@@ -13,25 +13,10 @@ def testapp():
     return app.Holocron()
 
 
-@pytest.fixture(scope='function')
-def run_processor():
-    streams = []
-
-    def run(*args, **kwargs):
-        streams.append(prettyuri.process(*args, **kwargs))
-        return streams[-1]
-
-    yield run
-
-    for stream in streams:
-        with pytest.raises(StopIteration):
-            next(stream)
-
-
-def test_document(testapp, run_processor):
+def test_item(testapp):
     """Prettyuri processor has to work!"""
 
-    stream = run_processor(
+    stream = prettyuri.process(
         testapp,
         [
             {
@@ -44,15 +29,18 @@ def test_document(testapp, run_processor):
             'destination': os.path.join('about', 'cv', 'index.html'),
         }
 
+    with pytest.raises(StopIteration):
+        next(stream)
+
 
 @pytest.mark.parametrize('index', [
     'index.html',
     'index.htm',
 ])
-def test_document_index(testapp, run_processor, index):
-    """Prettyuri processor has to ignore index documents."""
+def test_item_index(testapp, index):
+    """Prettyuri processor has to ignore index items."""
 
-    stream = run_processor(
+    stream = prettyuri.process(
         testapp,
         [
             {
@@ -65,68 +53,28 @@ def test_document_index(testapp, run_processor, index):
             'destination': os.path.join('about', 'cv', index),
         }
 
+    with pytest.raises(StopIteration):
+        next(stream)
 
-def test_param_when(testapp, run_processor):
-    """Prettyuri processor has to ignore non-targeted documents."""
 
-    stream = run_processor(
+@pytest.mark.parametrize('amount', [0, 1, 2, 5, 10])
+def test_item_many(testapp, amount):
+    """Prettyuri processor has to work with stream."""
+
+    stream = prettyuri.process(
         testapp,
         [
             {
-                'source': '0.txt',
-                'destination': '0.txt',
-            },
-            {
-                'source': '1.md',
-                'destination': os.path.join('1', 'index.html'),
-            },
-            {
-                'source': '2',
-                'destination': '2.html',
-            },
-            {
-                'source': '3.markdown',
-                'destination': '3.html',
-            },
-        ],
-        when=[
-            {
-                'operator': 'match',
-                'attribute': 'source',
-                'pattern': r'.*\.(markdown|mdown|mkd|mdwn|md)$',
-            },
+                'destination': os.path.join('about', '%d.html' % i),
+            }
+            for i in range(amount)
         ])
 
-    assert next(stream) == \
-        {
-            'source': '0.txt',
-            'destination': '0.txt',
-        }
+    for i in range(amount):
+        assert next(stream) == \
+            {
+                'destination': os.path.join('about', str(i), 'index.html'),
+            }
 
-    assert next(stream) == \
-        {
-            'source': '1.md',
-            'destination': os.path.join('1', 'index.html'),
-        }
-
-    assert next(stream) == \
-        {
-            'source': '2',
-            'destination': '2.html',
-        }
-
-    assert next(stream) == \
-        {
-            'source': '3.markdown',
-            'destination': os.path.join('3', 'index.html'),
-        }
-
-
-@pytest.mark.parametrize('params, error', [
-    ({'when': [42]}, 'when: unsupported value'),
-])
-def test_param_bad_value(testapp, params, error):
-    """Prettyuri processor has to validate input parameters."""
-
-    with pytest.raises(ValueError, match=error):
-        next(prettyuri.process(testapp, [], **params))
+    with pytest.raises(StopIteration):
+        next(stream)
