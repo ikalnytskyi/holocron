987a134d3fe9160800c6bb9ea6b59569048c97cb
Ihor Kalnytskyi <ihor@kalnytskyi.com>
2019-01-12 22:18:03 +0200

    Add 'when' processor
    When processor is a new processor wrapper that is designed to
    conditionally pass only a subset of items from a stream through a
    certain processor. This is very handy because it removes burden from
    processor authors to implement *condition evaluation logic*.

--- holocron/ext/processors/_misc.py
+++ holocron/ext/processors/_misc.py
@@ -1,6 +1,5 @@
 """Various miscellaneous functions to make code easier to read & write."""
 
-import re
 import collections
 import inspect
 import functools
@@ -9,37 +8,6 @@ import urllib.parse
 import jsonpointer
 import schema
 import dooku.conf
-
-
-_evaluators = {
-    'match':
-        lambda attribute, pattern, document: (
-            bool(re.match(pattern, document[attribute]))
-        ),
-}
-
-
-def evalcondition(condition, document):
-    parameters = dict(condition.copy(), document=document)
-    name = parameters.pop('operator')
-    return _evaluators[name](**parameters)
-
-
-def iterdocuments(documents, when):
-    for document, is_matched in iterdocuments_ex(documents, when):
-        if is_matched:
-            yield document
-
-
-def iterdocuments_ex(documents, when):
-    for document in documents:
-        is_matched = True
-
-        if when is not None:
-            if any((not evalcondition(cond, document) for cond in when)):
-                is_matched = False
-
-        yield document, is_matched
 
 
 def resolve_json_references(value, context, keep_unknown=True):
---
+++ holocron/ext/processors/when.py
@@ -0,0 +1,55 @@
+"""Pass a stream item to a processor based on condition."""
+
+import re
+import collections
+
+import jinja2
+
+
+def _re_match(value, pattern, flags=0):
+    return re.match(pattern, value, flags)
+
+
+class _WhenEvaluator:
+    """Evaluate a python-like expression using Jinja2 syntax.
+
+    When processor requires some means to evaluate string conditions. Turns
+    out there're not so many simple and safe solutions, so we decided to go
+    same approach Ansible went and use Jinja2 to evaluate expressions. It's
+    safe, fast, extensible, and we already have dependency on Jinja2.
+    """
+
+    def __init__(self):
+        self._env = jinja2.Environment()
+        self._env.filters.update({'match': _re_match})
+
+    def eval(self, when, **context):
+        template = self._env.from_string(
+            '{%% if %s %%}true{%% endif %%}' % when)
+        return template.render(**context) == 'true'
+
+
+def process(app, stream, *, when, processor):
+    untouched = collections.deque()
+    evaluator = _WhenEvaluator()
+
+    def smartstream():
+        for item in stream:
+            if all(evaluator.eval(cond, item=item) for cond in when):
+                yield item
+            else:
+                untouched.append(item)
+
+    for item in app.invoke_processors(smartstream(), [processor]):
+        # Some untouched items may be collected during an attempt to retrieve
+        # at least one item from a processor. In order to preserve relative
+        # order between items from an original input stream and items produced
+        # by a processor, we need to yield these untouched items first.
+        while untouched:
+            yield untouched.popleft()
+        yield item
+
+    # Untouched collection may contain some items if app.invoke_processors()
+    # drained an input stream without yielding a new item. Ensure they are
+    # propagated down the stream.
+    yield from untouched
