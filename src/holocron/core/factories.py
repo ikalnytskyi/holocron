8036457bce6966c3833d90a64993aeeb43b6c78f
Ihor Kalnytskyi <ihor@kalnytskyi.com>
2019-03-02 20:19:35 +0200

    Drop setuptools based discovering
    It was decided to do not proceed with stevedore and setuptools based
    discovering. There are few reasons why.
    
     * First, Holocron needs another way to *load* processors because we
       want to support loading processors from some user directory.
    
     * Second, it's hard to imagine a Holocron blog without a configuration
       file because pretty much everybody need to set at least a blog name.
       If we have a configuration anyway, why not to ask users to explicitly
       specify what plugins they want to use?
    
     * Third, because of security reasons we better not rely on environment
       and load only what's specified by a user. We stevedore approach we
       used to load every processor discovered in the system.

--- src/holocron/core/factories.py
+++ src/holocron/core/factories.py
@@ -1,8 +1,7 @@
 """Factory functions to create core instances."""
 
-import stevedore
-
 from . import Application
+from ..processors import import_processors
 
 
 def create_app(metadata, processors=None, pipes=None):
@@ -10,11 +9,23 @@ def create_app(metadata, processors=None, pipes=None):
 
     instance = Application(metadata)
 
-    for ext in stevedore.ExtensionManager(namespace="holocron.processors"):
-        instance.add_processor(ext.name, ext.plugin)
-
-    for ext in stevedore.ExtensionManager(namespace="holocron.pipes"):
-        instance.add_pipe(ext.name, ext.plugin)
+    import_processors.process(instance, [], imports=[
+        "archive = holocron.processors.archive:process",
+        "commit = holocron.processors.commit:process",
+        "feed = holocron.processors.feed:process",
+        "frontmatter = holocron.processors.frontmatter:process",
+        "jinja2 = holocron.processors.jinja2:process",
+        "markdown = holocron.processors.markdown:process",
+        "metadata = holocron.processors.metadata:process",
+        "pipe = holocron.processors.pipe:process",
+        "prettyuri = holocron.processors.prettyuri:process",
+        "restructuredtext = holocron.processors.restructuredtext:process",
+        "sitemap = holocron.processors.sitemap:process",
+        "source = holocron.processors.source:process",
+        "todatetime = holocron.processors.todatetime:process",
+        "when = holocron.processors.when:process",
+        "import-processors = holocron.processors.import_processors:process",
+    ])
 
     for name, processor in (processors or {}).items():
         instance.add_processor(name, processor)
