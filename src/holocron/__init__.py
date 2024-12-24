b8f23b55af47c42778fafbadb50fd8064066a78d
Ihor Kalnytskyi <ihor@kalnytskyi.com>
2024-12-25 00:33:36 +0200

    Bump Ruff to 0.8
    Over the years Ruff became a much more solid choice for projects. It's
    about time we update it.

--- src/holocron/__init__.py
+++ src/holocron/__init__.py
@@ -2,4 +2,4 @@
 
 from ._core import Application, Item, WebSiteItem, create_app
 
-__all__ = ["Application", "create_app", "Item", "WebSiteItem"]
+__all__ = ["Application", "Item", "WebSiteItem", "create_app"]
--- src/holocron/_core/__init__.py
+++ src/holocron/_core/__init__.py
@@ -4,4 +4,4 @@ from .application import Application
 from .factories import create_app
 from .items import Item, WebSiteItem
 
-__all__ = ["Application", "create_app", "Item", "WebSiteItem"]
+__all__ = ["Application", "Item", "WebSiteItem", "create_app"]
